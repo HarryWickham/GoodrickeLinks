0759d5b8cb5affad8ae8de4e0c4cf71a1d6cd7f2
Harry <harrywickham@live.co.uk>
2021-10-23 22:13:19 +0100

    added social icons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from "react";
 import Link from "./components/Link";
+import Socials from "./components/Socials";
 
 function App() {
   return (
     <div style={holder}>
-      <Link
-        Linktext="Lots and lots of text that wouldnt really ever be there"
-        Link=""
-        ImageURL="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F437969157326254081%2FKHXl64x5.png&f=1&nofb=1"
-      />
-      <Link
-        Linktext="a little bit of text"
-        Link=""
-        ImageURL="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F437969157326254081%2FKHXl64x5.png&f=1&nofb=1"
-      />
+      <div
+        style={{
+          justifyContent: "space-between",
+          flexGrow: 1,
+          flexDirection: "column",
+          display: "flex",
+        }}
+      >
+        <div
+          style={{
+            flexGrow: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Link
+            Linktext="Lots and lots of text that wouldnt really ever be there"
+            Link=""
+            ImageURL="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F437969157326254081%2FKHXl64x5.png&f=1&nofb=1"
+          />
+          <Link
+            Linktext="a little bit of text"
+            Link=""
+            ImageURL="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F437969157326254081%2FKHXl64x5.png&f=1&nofb=1"
+          />
+        </div>
+        <Socials style={{ alignSelf: "end" }} />
+      </div>
     </div>
   );
 }
 
 const holder = {
   display: "flex",
-  justifyContent: "center",
   alignItems: "center",
   flexDirection: "column",
   width: "100vw",
---
+++ src/components/Socials.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import { FaTwitter, FaInstagram, FaFacebookF } from "react-icons/fa";
+
+function Socials() {
+  const [FBhover, setFBHover] = useState();
+  const [Thover, setTHover] = useState();
+  const [Ihover, setIHover] = useState();
+
+  useEffect(() => {
+    if (FBhover) {
+      document.getElementById("FaFacebookF").style.color = "#1f6f2e";
+    } else {
+      document.getElementById("FaFacebookF").style.color = "black";
+    }
+    if (Thover) {
+      document.getElementById("FaTwitter").style.color = "#1f6f2e";
+    } else {
+      document.getElementById("FaTwitter").style.color = "black";
+    }
+    if (Ihover) {
+      document.getElementById("FaInstagram").style.color = "#1f6f2e";
+    } else {
+      document.getElementById("FaInstagram").style.color = "black";
+    }
+  });
+
+  return (
+    <div style={holder}>
+      <a
+        href="https://www.facebook.com/GoodrickeCollege/"
+        target="_blank"
+        onMouseEnter={() => setFBHover(true)}
+        onMouseLeave={() => setFBHover(false)}
+      >
+        <FaFacebookF style={icon} id="FaFacebookF" />
+      </a>
+      <a
+        href="https://www.instagram.com/goodrickecollege/"
+        target="_blank"
+        onMouseEnter={() => setIHover(true)}
+        onMouseLeave={() => setIHover(false)}
+      >
+        <FaInstagram style={icon} id="FaInstagram" />
+      </a>
+      <a
+        href="https://twitter.com/goodrickeyork"
+        target="_blank"
+        onMouseEnter={() => setTHover(true)}
+        onMouseLeave={() => setTHover(false)}
+      >
+        <FaTwitter style={icon} id="FaTwitter" />
+      </a>
+    </div>
+  );
+}
+
+const icon = {
+  margin: "8px",
+  height: "32px",
+  width: "auto",
+};
+
+const holder = {
+  display: "flex",
+  flexDirection: "row",
+  width: "100%",
+  justifyContent: "center",
+};
+
+export default Socials;
