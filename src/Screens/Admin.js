b64e5cd2ef282850b0e630a892ac2eb5e3cb1164
Harry <harrywickham@live.co.uk>
2021-11-13 11:32:24 +0000

    Add admin page

---
+++ src/Screens/Admin.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Admin() {
+  return <div>Hi from admin</div>;
+}
+
+export default Admin;
---
+++ src/Screens/Home.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+import Socials from "../components/Socials";
+import GetLinks from "../GetLinks";
+import GetEvents from "../GetEvents";
+
+function Home() {
+  return (
+    <div style={holder}>
+      <div style={center_container}>
+        <GetEvents style={{ alignItems: "center", display: "flex" }} />
+        <div style={link_holder}>
+          <GetLinks />
+        </div>
+        <Socials style={{ alignSelf: "end" }} />
+      </div>
+      <p style={{ textAlign: "center", margin: "2px" }}>
+        <b>
+          Designed, Developed and Maintained by{" "}
+          <a
+            style={{ color: "#1f6f2e", textDecoration: "none" }}
+            href="mailto:hew550@york.ac.uk"
+          >
+            Harry Wickham
+          </a>{" "}
+          @{" "}
+          <a
+            style={{ color: "#1f6f2e", textDecoration: "none" }}
+            href="https://yordevs.com/"
+          >
+            Yordevs
+          </a>
+        </b>
+      </p>
+    </div>
+  );
+}
+
+const holder = {
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+  width: "100vw",
+  height: "100vh",
+};
+
+const link_holder = {
+  flexGrow: 1,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+};
+
+const center_container = {
+  justifyContent: "space-between",
+  alignItems: "center",
+  flexGrow: 1,
+  flexDirection: "column",
+  display: "flex",
+};
+
+export default Home;
